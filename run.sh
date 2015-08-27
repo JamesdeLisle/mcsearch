1e9fb0717d09bfbb752ac679689dbd076fb1ac11
JamesdeLisle <lpko908@gmail.com>
2015-08-27 20:27:28 +0100

    rearrange folders

---
+++ run.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+#for ((n=0;n<20;n++))
+#    do
+#        cd ~/Dropbox/Work/MCSearch/numerics/1DSearch-v1.0/1DSearch-v1.0/source
+#        python -B search1DImag.py
+#        mv *.dat ~/Dropbox/Work/MCSearch/numerics/1DSearch-v1.0/1DSearch-v1.0/rundata
+#    done
+
+main()
+{
+    cd ~/Dropbox/Work/MCSearch/numerics/1DSearch-v1.0/1DSearch-v1.0/source
+    python -B search1DImag.py
+    mv *.dat ~/Dropbox/Work/MCSearch/numerics/1DSearch-v1.0/1DSearch-v1.0/rundata
+    cd ..
+}
+
+
+main
